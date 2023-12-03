a2c6f932ec3aba4da46680abce93338bd30cec56
quakk <quakk@localhost.localdomain>
2023-12-03 22:50:52 +0100

    Some operations

--- new_stack.c
+++ new_stack.c
@@ -14,40 +14,143 @@ t_stack *ft_new_stack(int content)
 
 void ft_add(t_stack **stack, int nb)
 {
-    t_stack *newNode;
-    
-    newNode = ft_new_stack(nb);
+    t_stack *newNode = ft_new_stack(nb);
+
     if (newNode)
     {
-        newNode->next = *stack;
-        *stack = newNode;
+        t_stack *temp = *stack;
+
+        if (temp)
+        {
+            while (temp->next != NULL)
+                temp = temp->next;
+
+            temp->next = newNode;
+            newNode->prev = temp;
+        }
+        else
+            *stack = newNode;
     }
 }
 
+
+
 void ft_print_stacks(t_stack *A, t_stack *B)
 {
-    if (A == NULL || A->next == NULL)
-        printf("Stack A: empty\n");
+    printf("\n");
+    if (A == NULL)
+        printf("Stack A: \n");
     else
-    {          
-        while (A)
+    {
+        while (A != NULL)
         {
-            printf("Stack A: %d\n", A->nb);
+            printf("Stack A: %ld\n", A->nb);
             A = A->next;
         }
     }
+
     printf("\n");
-    if (B == NULL || B->next == NULL)
-        printf("Stack B: empty\n");
+
+    if (B == NULL)
+        printf("Stack B: \n");
     else
     {
-        while (B)
+        while (B != NULL)
         {
-            printf("Stack B: %d\n", B->nb);
+            printf("Stack B: %ld\n", B->nb);
             B = B->next;
         }
     }
 }
+
+// void ft_swap_a(t_stack **stack)
+// {
+//     int temp;
+//     t_stack *first;
+//     t_stack *second;
+
+//     if(*stack && (*stack)->next)
+//     {
+//         first = *stack;
+//         second = (*stack)->next;
+//     }
+//     temp = first->nb;
+//     first->nb = second->nb;
+//     second->nb= temp;
+
+// }
+
+void ft_swap_a(t_stack **A)
+{
+    int temp;
+    t_stack *first;
+    t_stack *second;
+
+    if(*A && (*A)->next)
+    {
+        first = *A;
+        second = (*A)->next;
+    }
+    temp = first->nb;
+    first->nb = second->nb;
+    second->nb= temp;
+}
+
+void ft_push_b(t_stack **A, t_stack **B)
+{
+    if (*A != NULL)
+    {
+        t_stack *temp;
+
+        temp = *A;
+        *A = (*A)->next;
+        temp->next = *B;
+        *B = temp;
+    }
+    else
+        printf("Stack A: empty\n");
+}
+
+void ft_push_a(t_stack **B, t_stack **A)
+{
+    if (*B != NULL)
+    {
+        t_stack *temp;
+
+        temp = *B;
+        *B = (*B)->next;
+void ft_push_b(t_stack **A, t_stack **B)
+    }
+    else
+        printf("Stack B: empty\n");
+}
+
+void ft_swap_ab(t_stack **A , t_stack **B)
+{
+    int tempA;
+    int tempB;
+    t_stack *first;
+    t_stack *second;
+
+    if(*A && (*A)->next)
+    {
+        first = *A;
+        second = (*A)->next;
+    }
+    tempA= first->nb;
+    first->nb = second->nb;
+    second->nb= tempA;
+
+    if(*B && (*B)->next)
+    {
+        first = *B;
+        second = (*B)->next;
+    }
+    tempB = first->nb;
+    first->nb = second->nb;
+    second->nb= tempB;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -69,5 +172,21 @@ int main(int argc, char **argv)
         i++;
     }
     
+    ft_print_stacks(A, B);
+
+    ft_push_b(&A, &B);
+
+    ft_print_stacks(A, B);
+
+    ft_push_b(&A, &B);
+
+    ft_print_stacks(A, B);
+
+    ft_swap_ab(&A, &B);
+
+    ft_print_stacks(A, B);
+
+    ft_push_a(&B, &A);
+
     ft_print_stacks(A, B);
 }
--- swaps.c
+++ swaps.c
@@ -1,17 +1,59 @@
 #include "push_swap.h"
 
-void ft_swap_a(t_node **stack)
+void ft_swap_a(t_stack **A)
 {
     int temp;
-    t_node *first;
-    t_node *second;
+    t_stack *first;
+    t_stack *second;
 
-    if(*stack && (*stack)->next)
+    if(*A && (*A)->next)
     {
-        first = *stack;
-        second = (*stack)->next;
+        first = *A;
+        second = (*A)->next;
     }
     temp = first->nb;
     first->nb = second->nb;
     second->nb= temp;
+}
+
+void ft_swap_b(t_stack **B)
+{
+    int temp;
+    t_stack *first;
+    t_stack *second;
+
+    if(*B && (*B)->next)
+    {
+        first = *B;
+        second = (*B)->next;
+    }
+    temp = first->nb;
+    first->nb = second->nb;
+    second->nb= temp;
+}
+
+void ft_swap_ab(t_stack **A , t_stack **B)
+{
+    int tempA;
+    int tempB;
+    t_stack *first;
+    t_stack *second;
+
+    if(*A && (*A)->next)
+    {
+        first = *A;
+        second = (*A)->next;
+    }
+    tempA= first->nb;
+    first->nb = second->nb;
+    second->nb= tempA;
+
+    if(*B && (*B)->next)
+    {
+        first = *B;
+        second = (*B)->next;
+    }
+    tempB = first->nb;
+    first->nb = second->nb;
+    second->nb= tempB;
 }
